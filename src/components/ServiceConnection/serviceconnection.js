50b9a5a366ffe5c078bc41e0ae3dbbb2bbaa210b
Kshitij Budholiya <kshitijbudholiya2006@gmail.com>
2026-09-18 12:02:16 +0530

    push

--- src/components/ServiceConnection/serviceconnection.js
+++ src/components/ServiceConnection/serviceconnection.js
@@ -8,7 +8,7 @@ const NO_TOKEN_VALUES = ["0", "1", "", null, undefined];
 ========================================================= */
 
 let APL_LINK = "https://omhonda.triosoft.ai/";
-APL_LINK = "http://localhost:8000/";
+// APL_LINK = "http://localhost:8000/";
 
 const AUDIO_BASE_URL = "/media/call_recordings/";
 
